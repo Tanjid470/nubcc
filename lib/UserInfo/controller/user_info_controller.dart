5db4cc1d722650f484b35f78b41ece0391f06e94
Tanjid470 <tanjidamran11@gmail.com>
2024-10-14 10:15:38 +0600

    reformat and refactor

--- lib/UserInfo/controller/user_info_controller.dart
+++ lib/UserInfo/controller/user_info_controller.dart
@@ -2,11 +2,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 class UserInfoController extends GetxController{
-  TextEditingController whatsappNumberController = TextEditingController();
-  int? selectedValueCategory;
-  int? selectedValueDepartment;
-  int? selectedValueSemester;
-  int? selectedValueSession;
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController studentIdController =TextEditingController();
+  int? semesterID;
+  int? bloodGroup;
+
 
   @override
   void onInit() async{
@@ -15,7 +18,7 @@ class UserInfoController extends GetxController{
   }
 
 
-  TextEditingController nameController = TextEditingController();
+
   RxString userNameErrorMassage = ''.obs;
   RxBool inputValidUserName = false.obs;
   userNameInputValidation(){
@@ -30,7 +33,7 @@ class UserInfoController extends GetxController{
     update();
   }
 
-  TextEditingController emailController = TextEditingController();
+
   RxString userEmailErrorMassage = ''.obs;
   RxBool inputValidUserEmail = false.obs;
 
@@ -55,18 +58,18 @@ class UserInfoController extends GetxController{
     update();
   }
 
-  RxString phoneNumberErrorMassage = ''.obs;
-  RxBool inputValidNumber = false.obs;
+
 
   RxBool saveButtonEnableFlag =false.obs;
 
 
   void saveButtonEnable(){
-    if( selectedValueCategory != null
-        && selectedValueDepartment != null
-        && selectedValueSemester != null
+    if( semesterID != null
+        && studentIdController.text.isNotEmpty
         && emailController.text.isNotEmpty
-        && nameController.text.isNotEmpty){
+        && nameController.text.isNotEmpty
+        && phoneController.text.isNotEmpty
+    ){
       saveButtonEnableFlag.value = true;
     }
     else{
@@ -74,17 +77,21 @@ class UserInfoController extends GetxController{
     }
   }
 
+
+  RxString phoneNumberErrorMassage = ''.obs;
+  RxBool inputValidNumber = false.obs;
+
   numberInputValidation(){
     RegExp regExp = RegExp(r'(^(?:\+?88|0088)?01[3-9]\d{8}$)');
-    if(whatsappNumberController.text.isEmpty){
+    if(phoneController.text.isEmpty){
       phoneNumberErrorMassage('আপনার মোবাইল নাম্বার দিন');
       inputValidNumber.value = false;
     }
-    else if(whatsappNumberController.text.length > 1 && whatsappNumberController.text.length < 11){
+    else if(phoneController.text.length > 1 && phoneController.text.length < 11){
       phoneNumberErrorMassage('ফোন নাম্বার ১১ ডিজিটের হতে হবে');
       inputValidNumber.value = false;
     }
-    else if(!regExp.hasMatch(whatsappNumberController.text)){
+    else if(!regExp.hasMatch(phoneController.text)){
       phoneNumberErrorMassage.value = 'সঠিক মোবাইল নম্বর দিন';
       inputValidNumber.value = false;
     }
@@ -94,8 +101,5 @@ class UserInfoController extends GetxController{
     }
     update();
   }
-
-
-
 
 }
