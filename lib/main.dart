636a61b577cefb28601afb1306b05852f3c85974
Tanjid470 <tanjidamran11@gmail.com>
2024-10-14 12:29:57 +0600

    okay

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'UserInfo/view/user_info_screen.dart';
 import 'api.dart';
 
-void main() {
-  UserSheetApi.inti();
+void main() async{
+  await UserSheetApi.inti();
   runApp(const MyApp());
 }
 
@@ -14,12 +14,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'NUBCC',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
       home:  const UserInfoScreen(),
+      // builder: FlutterSmartDialog.init(
+      //   toastBuilder: (String msg) => Center(
+      //     child: Container(
+      //       padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+      //       decoration: BoxDecoration(borderRadius: BorderRadius.circular(25),
+      //           color: Colors.red),
+      //       child: Text(msg,
+      //         style:  const TextStyle(
+      //           color: Colors.white,
+      //           fontSize: 18,
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      //   loadingBuilder: (String msg) => LoadingAnimationWidget.hexagonDots(
+      //       color: Colors.green,
+      //       size:  20
+      //   ),
+      // ),
     );
   }
 }
