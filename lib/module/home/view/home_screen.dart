4733a659322e161a5fc91e346d91fdd4397ea868
Tanjid470 <tanjidamran11@gmail.com>
2024-10-14 16:21:06 +0600

    demo home screen

---
+++ lib/module/home/view/home_screen.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+        child: Center(child: Text("Home"),),
+      ),
+    );
+  }
+}
