2c31197749a6488f7325607c3ed1062bbb3bd96d
Tanjid470 <tanjidamran11@gmail.com>
2024-10-14 16:21:21 +0600

    initialRoute: AppPages.INITIAL,       getPages: AppPages.routes,

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+import 'package:get/get.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'UserInfo/view/user_info_screen.dart';
+import 'package:nubcc/routes/app_pages.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'google_sheet_init.dart';
+import 'module/UserInfo/view/user_info_screen.dart';
 
+late SharedPreferences preferences;
+int? isInitScreen;
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await GoogleSheetInit().inti();
+  preferences = await SharedPreferences.getInstance();
+  isInitScreen = preferences.getInt('initScreen');
   runApp(const MyApp());
 }
 
@@ -16,13 +24,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+
       title: 'NUBCC',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
       home:  const UserInfoScreen(),
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
       builder: FlutterSmartDialog.init(
         toastBuilder: (String msg) => Center(
           child: Container(
