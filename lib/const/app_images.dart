6722be1b772c5e95117ca1d3f24892fad822f9f9
Tanjid470 <tanjidamran11@gmail.com>
2024-10-16 15:26:24 +0600

    archive

--- lib/const/app_images.dart
+++ lib/const/app_images.dart
@@ -1,4 +1,8 @@
 class AppImages{
   AppImages._();
   static String facultyImage = "assets/images/faculty.png";
+  static String club = "assets/images/club.png";
+  static String cr = "assets/images/cr.png";
+  static String crMulti = "assets/images/crmulti.png";
+  static String admin = "assets/images/admin.png";
 }
