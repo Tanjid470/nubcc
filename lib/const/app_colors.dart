9bf9a8a4e630c8137c78ba733d9fc8fc510180f0
Tanjid470 <tanjidamran11@gmail.com>
2024-10-17 15:50:24 +0600

    new colors added

--- lib/const/app_colors.dart
+++ lib/const/app_colors.dart
@@ -4,6 +4,7 @@ class AppColor{
   AppColor._();
 
   static Color baseColor = const Color(0xff0346ae);
+  static Color baseColorShade700 = const Color(0xff0346ae).withOpacity(.7);
   static Color baseColorShade500 = const Color(0xff0346ae).withOpacity(.5);
   static Color baseColorShade300 = const Color(0xff0346ae).withOpacity(.3);
   static Color iconColor = const Color(0xff9BC6FF);
