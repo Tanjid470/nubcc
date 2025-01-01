202ff59fcf9ede6ac26c23e8c29f420bd68ed3c9
Tanjid470 <tanjidamran11@gmail.com>
2025-01-01 22:40:56 +0600

    Firebase Notification message work successfully.

--- lib/firebase/firebase_message_api.dart
+++ lib/firebase/firebase_message_api.dart
@@ -1,6 +1,20 @@
 
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+
+
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await setupFlutterNotifications();
+  // showFlutterNotification(message);
+  // // If you're going to use other Firebase services in the background, such as Firestore,
+  // // make sure you call `initializeApp` before using other Firebase services.
+  print('Handling a background message ${message.messageId}');
+  print('Handling a background message ${message.notification?.title}');
+  print('Handling a background message ${message.notification?.body}');
+  print('Handling a background message ${message.data}');
+}
 
 class FirebaseMessageAPI{
   final _firebaseMessaging = FirebaseMessaging.instance;
@@ -9,6 +23,7 @@ class FirebaseMessageAPI{
     await _firebaseMessaging.requestPermission();
     final fCMToken = await _firebaseMessaging.getToken();
     print('Token : $fCMToken');
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
+import 'package:firebase_app_installations/firebase_app_installations.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:nubcc/routes/app_pages.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'const/font_constant.dart';
+import 'firebase/firebase_message_api.dart';
 import 'google_sheet_init.dart';
 import 'module/UserInfo/view/user_info_screen.dart';
 
@@ -13,10 +19,56 @@ int? isInitScreen;
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+// Load the google-services.json file as a string
+  String googleServicesJsonString =
+  await rootBundle.loadString('android\\app\\google-services.json');
+  print("google_Services_Json_String: $googleServicesJsonString");
+
+  // Parse the JSON String into a JSON
+  final googleServicesJson = jsonDecode(googleServicesJsonString);
+  final projectId = googleServicesJson['project_info']["project_id"];
+  print("projectId: $projectId");
+  final messagingSenderId =
+  googleServicesJson['project_info']["project_number"];
+  print("messagingSenderId: $messagingSenderId");
+  final storageBucket = googleServicesJson['project_info']["storage_bucket"];
+  print("storageBucket: $storageBucket");
+  final appId =
+  googleServicesJson['client'][0]["client_info"]["mobilesdk_app_id"];
+  print("appId: $appId");
+  final apiKey = googleServicesJson['client'][0]["api_key"][0]["current_key"];
+  print("apiKey: $apiKey");
+
+  // Create a FirebaseOptions object from the extracted configuration
+  FirebaseOptions options = FirebaseOptions(
+    apiKey: apiKey,
+    appId: appId,
+    messagingSenderId: messagingSenderId,
+    projectId: projectId,
+    storageBucket: storageBucket,
+  );
+
+  // Initialize Firebase with the options
+  await Firebase.initializeApp(options: options);
+  _getInstallationId();
+  print('Firebase app is initialized!');
+  await FirebaseMessageAPI().initNotifications();
+
   await GoogleSheetInit().inti();
   preferences = await SharedPreferences.getInstance();
   isInitScreen = preferences.getInt('initScreen');
   runApp(const MyApp());
+}
+
+String? _installationId;
+
+Future<void> _getInstallationId() async {
+  try {
+    _installationId = await FirebaseInstallations.instance.getId();
+    print('Installation ID-----------------------------------------------------------: $_installationId');
+  } catch (e) {
+    print('Error getting Installation ID----------------------------------------: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
