4661d3f266ebe725fcb5cc43ab3596c1183da418
Tanjid470 <tanjidamran11@gmail.com>
2025-01-02 20:59:10 +0600

    added new images path

--- lib/const/app_images.dart
+++ lib/const/app_images.dart
@@ -11,4 +11,13 @@ class AppImages{
   static String unselectedHome = "assets/images/unselectedHome.png";
   static String unselectedLocation = "assets/images/unselectedLocation.png";
   static String unselectedProfile = "assets/images/unselectedProfile.png";
+
+  static String splashOne = "assets/splash/s1.jpg";
+  static String splashTwo = "assets/splash/s2.jpg";
+  static String splashThree = "assets/splash/s3.jpg";
+  static String splashFour = "assets/splash/s4.jpg";
+  static String splashFive = "assets/splash/s5.jpg";
+  static String splashSix = "assets/splash/s6.jpg";
+  static String splashSeven = "assets/splash/s7.jpg";
+  static String splashEight = "assets/splash/s8.jpg";
 }
