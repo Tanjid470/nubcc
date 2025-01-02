61f3e6d2a59f7db81feea3a6475b417a4eb567a3
Tanjid470 <tanjidamran11@gmail.com>
2025-01-02 15:39:32 +0600

    Firebase Notification on tap show on app successfully.

--- lib/firebase/firebase_message_api.dart
+++ lib/firebase/firebase_message_api.dart
@@ -1,29 +1,39 @@
 
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:nubcc/firebase/view/notification_screen.dart';
+import 'package:nubcc/main.dart';
 
 
-Future<void> handleBackgroundMessage(RemoteMessage message) async {
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // await setupFlutterNotifications();
-  // showFlutterNotification(message);
-  // // If you're going to use other Firebase services in the background, such as Firestore,
-  // // make sure you call `initializeApp` before using other Firebase services.
-  print('Handling a background message ${message.messageId}');
-  print('Handling a background message ${message.notification?.title}');
-  print('Handling a background message ${message.notification?.body}');
-  print('Handling a background message ${message.data}');
-}
+  Future<void> handleBackgroundMessage(RemoteMessage message) async {
 
-class FirebaseMessageAPI{
-  final _firebaseMessaging = FirebaseMessaging.instance;
+  }
+  void handleMessage(RemoteMessage? message) async {
+    if(message == null ) return;
+    navigatorKey.currentState?.pushNamed(
+      NotificationScreen.route,
+      arguments: message,
+    );
+  }
 
-  Future<void> initNotifications() async{
-    await _firebaseMessaging.requestPermission();
-    final fCMToken = await _firebaseMessaging.getToken();
-    print('Token : $fCMToken');
+  Future intiPushNotification() async{
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
 
-}
+  class FirebaseMessageAPI{
+    final _firebaseMessaging = FirebaseMessaging.instance;
+
+    Future<void> initNotifications() async{
+      await _firebaseMessaging.requestPermission();
+      final fCMToken = await _firebaseMessaging.getToken();
+      print('Token : $fCMToken');
+      intiPushNotification();
+    }
+
+  }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-
-import 'package:firebase_app_installations/firebase_app_installations.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,47 +9,20 @@ import 'package:nubcc/routes/app_pages.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'const/font_constant.dart';
 import 'firebase/firebase_message_api.dart';
+import 'firebase/view/notification_screen.dart';
 import 'google_sheet_init.dart';
 import 'module/UserInfo/view/user_info_screen.dart';
 
 late SharedPreferences preferences;
 int? isInitScreen;
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-// Load the google-services.json file as a string
-  String googleServicesJsonString =
-  await rootBundle.loadString('android\\app\\google-services.json');
-  print("google_Services_Json_String: $googleServicesJsonString");
 
-  // Parse the JSON String into a JSON
-  final googleServicesJson = jsonDecode(googleServicesJsonString);
-  final projectId = googleServicesJson['project_info']["project_id"];
-  print("projectId: $projectId");
-  final messagingSenderId =
-  googleServicesJson['project_info']["project_number"];
-  print("messagingSenderId: $messagingSenderId");
-  final storageBucket = googleServicesJson['project_info']["storage_bucket"];
-  print("storageBucket: $storageBucket");
-  final appId =
-  googleServicesJson['client'][0]["client_info"]["mobilesdk_app_id"];
-  print("appId: $appId");
-  final apiKey = googleServicesJson['client'][0]["api_key"][0]["current_key"];
-  print("apiKey: $apiKey");
-
-  // Create a FirebaseOptions object from the extracted configuration
-  FirebaseOptions options = FirebaseOptions(
-    apiKey: apiKey,
-    appId: appId,
-    messagingSenderId: messagingSenderId,
-    projectId: projectId,
-    storageBucket: storageBucket,
-  );
-
-  // Initialize Firebase with the options
+  FirebaseOptions options = await firebaseInit();
   await Firebase.initializeApp(options: options);
-  _getInstallationId();
-  print('Firebase app is initialized!');
   await FirebaseMessageAPI().initNotifications();
 
   await GoogleSheetInit().inti();
@@ -60,15 +31,26 @@ void main() async{
   runApp(const MyApp());
 }
 
-String? _installationId;
+Future<FirebaseOptions> firebaseInit() async{
 
-Future<void> _getInstallationId() async {
-  try {
-    _installationId = await FirebaseInstallations.instance.getId();
-    print('Installation ID-----------------------------------------------------------: $_installationId');
-  } catch (e) {
-    print('Error getting Installation ID----------------------------------------: $e');
-  }
+  String googleServicesJsonString = await rootBundle.loadString('android\\app\\google-services.json');
+  final googleServicesJson = jsonDecode(googleServicesJsonString);
+  final projectId = googleServicesJson['project_info']["project_id"];
+
+  final messagingSenderId = googleServicesJson['project_info']["project_number"];
+  final storageBucket = googleServicesJson['project_info']["storage_bucket"];
+  final appId = googleServicesJson['client'][0]["client_info"]["mobilesdk_app_id"];
+  final apiKey = googleServicesJson['client'][0]["api_key"][0]["current_key"];
+
+  FirebaseOptions options = FirebaseOptions(
+    apiKey: apiKey,
+    appId: appId,
+    messagingSenderId: messagingSenderId,
+    projectId: projectId,
+    storageBucket: storageBucket,
+  );
+
+  return options;
 }
 
 class MyApp extends StatelessWidget {
@@ -108,6 +90,10 @@ class MyApp extends StatelessWidget {
         ),
       ),
       navigatorObservers: [FlutterSmartDialog.observer],
+      navigatorKey: navigatorKey,
+      routes: {
+        NotificationScreen.route: (context) => const NotificationScreen()
+      },
     );
   }
 }
